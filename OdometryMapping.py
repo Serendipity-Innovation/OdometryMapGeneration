dae168bd61cef273f5d683e4b5587f3058494563
Radagast-the-infinite <ng.kyle101@gmail.com>
2020-05-22 14:48:26 -0700

    Added Display for Graph(functional now)

--- OdometryMapping.py
+++ OdometryMapping.py
@@ -1,6 +1,7 @@
 #change path directory per computer
 import matplotlib.pyplot as plt
 import csv
+import string
 
 def getDataX(graphDataPath):
     x = []
@@ -33,8 +34,11 @@ def readFile(path):
         return file.read()
 
 def getVersion():
-    versionStringFile = readFile("/Users/JustKyle-ngaround/StudioProjects/OdometryMapGeneration/version.txt")
+    versionStringFile = readFile("version.txt")
     for line in versionStringFile:
+      if line.isspace():
+        pass
+      else:
         version = line #will repeat all the way to the end, the last line is the version number
     return version
     
@@ -43,28 +47,35 @@ def makeGraph(graphDataPath):
     xList = getDataX(graphDataPath)
     yList = getDataY(graphDataPath)
     isDetectGermsList = getDataIsDetectGerms(graphDataPath)
-# plot the data
+    print("xList is " + str(xList))
+    print("yList is " + str(yList))
+# plot the data first
+    noGermsDetected, = plt.plot(xList, yList, label = "No Germs Detected", color = "green")
+# add dots to where germs are found
+    detectGermsListX = []
+    detectGermsListY = []
     for i in range(len(xList)):
         xCoordinate = xList[i]
         yCoordinate = yList[i]
-        isDetectGermsList = isDetectGermsList[i]
-        if isDetectGermsList == "true":
-            plt.plot(xCoordinate,yCoordinate,marker="o")
-        else:
-            plt.plot(xCoordiante,yCoordinate,maker="-")
-        
+        isDetectGerms = isDetectGermsList[i]
+        if isDetectGerms == "true":
+          detectGermsListX.append(xCoordinate)
+          detectGermsListY.append(yCoordinate)
+    germDetectedLabel, = plt.plot(detectGermsListX, detectGermsListY, marker="o", linewidth = 5, label = "Germs Detected", color="red")
+
+# Add titles, legends, etc
     plt.title("Germ Detection Location in Robot Path")
     plt.xlabel("X-Axis")
     plt.ylabel("Y-Axis")
+    plt.legend(handles = [germDetectedLabel, noGermsDetected])
+    #plt.legend(handles = noGermsDetected)
     plt.show()
 
 def main():
     version = getVersion()
-    makeGraph("/Users/JustKyle-ngaround/Desktop/odometryGraphData/odometryMapData"
-                + version + ".txt")
+    makeGraph("odometryGraphData" + version + ".txt")
     
-    
-    
+main()
     
 
         
